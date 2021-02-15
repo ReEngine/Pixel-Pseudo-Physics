145ab8bce3dbe6b359e41635fde6455b5ee8e382
Игорь Амелин <amelin.it@gmail.com>
2021-02-15 15:18:55 +0300

    Restarted from scratch

--- Pixel.cs
+++ Pixel.cs
@@ -4,189 +4,57 @@ using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 
-
 namespace SFMLTryout
 {
     class Pixel
     {
+
+        const byte Generator = 0;
+        const byte Air = 1;
+        const byte Sand = 2;
+
         public Color color;
-        Vector2i position;
-        public Material Material;
-        public Pixel(Material material, int x, int y)
+        public byte Material;
+
+        public Pixel(byte material)
         {
             this.Material = material;
-            Random rnd = new Random();
-            if (this.Material == Material.Sand)
-            {
-                byte _RG = Convert.ToByte(rnd.Next(200, 255));
-                this.color = new Color(_RG, _RG, Convert.ToByte(rnd.Next(100, _RG)));
-            }
-            if (this.Material == Material.Generator)
+            if (material == 0)
                 this.color = Color.Magenta;
-            if (this.Material == Material.Air)
+            if (material == 1)
                 this.color = Color.Black;
-            this.position = new Vector2i(x, y);
-        }
-
-        void GetColor()
-        {
-
-        }
-
-        public void Update()
-        {
-            if (this.Material == Material.Sand)
+            if (material == 2)
             {
-                if (Down(Material.Air))
+                //this.color = Color.Yellow;
+                Random rnd = new Random();
+                byte _RG = Convert.ToByte(rnd.Next(200, 255));
+                this.color = new Color(_RG, _RG, Convert.ToByte(rnd.Next(100,_RG)));
+
+            }
+        }
+        public void Update(uint x, uint y)
+        {
+            if (this.Material == Sand)
+            {
+                if (y + 1 < Program._Height)
                 {
-                    this.MoveDown();
+                    if (Down(Air,x,y))
+                    {
+                        MoveDown(x,y);
+                    }
                 }
             }
-            if (this.Material == Material.Generator)
-            {
-                if (Down(Material.Air) & Program.placing)
-                    PlaceDown(new Pixel(Program.mP, this.position.X, this.position.Y));
-            }
+        }
+        public bool Down(byte material,uint x, uint y)
+        {
+            return (Program.field[x, y + 1].Material == material & y < Program._Height);
         }
 
-
-        bool Up(Material material)
+        public void MoveDown(uint x, uint y)
         {
-            if (Program.field[this.position.X, this.position.Y - 1].Material == material)
-                return true;
-            else return false;
+            Pixel tmp = Program.field[x, y + 1];
+            Program.field[x, y + 1] = this;
+            Program.field[x, y] = tmp;
         }
-        bool Down(Material material)
-        {
-            if (this.position.Y + 2 < Program._Height)
-                if (Program.field[this.position.X, this.position.Y + 1].Material == material)
-                    return true;
-                else return false;
-            else return false;
-        }
-        bool Left(Material material)
-        {
-            if (Program.field[this.position.X - 1, this.position.Y].Material == material)
-                return true;
-            else return false;
-        }
-        bool Right(Material material)
-        {
-            if (Program.field[this.position.X + 1, this.position.Y].Material == material)
-                return true;
-            else return false;
-        }
-        bool UpLeft(Material material)
-        {
-            if (Program.field[this.position.X - 1, this.position.Y - 1].Material == material)
-                return true;
-            else return false;
-        }
-        bool UpRight(Material material)
-        {
-            if (Program.field[this.position.X + 1, this.position.Y - 1].Material == material)
-                return true;
-            else return false;
-        }
-        bool DownLeft(Material material)
-        {
-            if (Program.field[this.position.X - 1, this.position.Y + 1].Material == material)
-                return true;
-            else return false;
-        }
-        bool DownRight(Material material)
-        {
-            if (Program.field[this.position.X + 1, this.position.Y - 1].Material == material)
-                return true;
-            else return false;
-        }
-        bool Around(Material material)
-        {
-            if (Program.field[this.position.X - 1, this.position.Y - 1].Material == material | Program.field[this.position.X + 0, this.position.Y - 1].Material == material | Program.field[this.position.X + 1, this.position.Y - 1].Material == material |
-                Program.field[this.position.X - 1, this.position.Y + 0].Material == material | /*  Placeholder    Placeholder    Placeholder    Placeholder    Placeholder */ Program.field[this.position.X + 1, this.position.Y + 0].Material == material |
-                Program.field[this.position.X - 1, this.position.Y + 1].Material == material | Program.field[this.position.X + 0, this.position.Y - 1].Material == material | Program.field[this.position.X + 1, this.position.Y + 1].Material == material)
-                return true;
-            else return false;
-        }
-
-        void MoveUp()
-        {
-            Program.field[this.position.X, this.position.Y - 1] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveDown()
-        {
-            Program.field[this.position.X, this.position.Y + 1] = this;
-            Program.field[this.position.X, this.position.Y] = new Pixel(Material.Air, this.position.X, this.position.Y);
-        }
-        void MoveLeft()
-        {
-
-            Program.field[this.position.X - 1, this.position.Y] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveRight()
-        {
-
-            Program.field[this.position.X + 1, this.position.Y] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveUpLeft()
-        {
-
-            Program.field[this.position.X - 1, this.position.Y - 1] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveUpRight()
-        {
-            Program.field[this.position.X + 1, this.position.Y - 1] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveDownLeft()
-        {
-            Program.field[this.position.X - 1, this.position.Y + 1] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-        void MoveDownRight()
-        {
-
-            Program.field[this.position.X + 1, this.position.Y + 1] = this;
-            Program.field[this.position.X, this.position.Y].Material = Material.Air;
-        }
-
-        void PlaceUp(Pixel pixel)
-        {
-
-        }
-        void PlaceDown(Pixel pixel)
-        {
-            Program.field[this.position.X, this.position.Y + 1] = new Pixel(Material.Sand, this.position.X, this.position.Y + 1);
-        }
-        void PlaceLeft(Pixel pixel)
-        {
-
-        }
-        void PlaceRight(Pixel pixel)
-        {
-
-        }
-        void PlaceUpLeft(Pixel pixel)
-        {
-
-        }
-        void PlaceUpRight(Pixel pixel)
-        {
-
-        }
-        void PlaceDownLeft(Pixel pixel)
-        {
-
-        }
-        void PlaceDownRight(Pixel pixel)
-        {
-
-        }
-
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,68 +9,49 @@ namespace SFMLTryout
 {
     class Program
     {
+        const byte Generator = 0;
+        const byte Air = 1;
+        const byte Sand = 2;
+
 
         public static int TestCounter = 0;
 
-        //const int _Width = 640;
-        //const int _Height = 480;
+        public static uint _SWidth = VideoMode.DesktopMode.Width;
+        public static uint _SHeight = VideoMode.DesktopMode.Height;
 
-        const int _SWidth = 1920;
-        const int _SHeight = 1080;
+        const uint _ResMult = 1010b;
 
-        const int _ResMult = 1;
+        public static uint _Width = _SWidth / _ResMult;
+        public static uint _Height = _SHeight / _ResMult;
 
-
-        public const int _Width = 192 * _ResMult;
-        public const int _Height = 108 * _ResMult;
-        Random rnd = new Random();
-
-        public static Vector2i position;
-        public static Color[,] ScreenBuffer = new Color[_Width, _Height];
         public static Texture MainViewPort = new Texture(_Width, _Height);
 
-        public static byte[] pixels = new byte[_Width * _Height * 4]; //_Width x _Height pixels x 4 bytes per pixel
-        public static Color[] cpixels = new Color[_Width * _Height];//intermediary step to keep everything clear
+        public static byte[] pixels = new byte[_Width * _Height * 4];
+        public static Color[] cpixels = new Color[_Width * _Height];
 
-        public static byte[,] fireField = new byte[_Width, _Height];
         public static Pixel[,] field = new Pixel[_Width, _Height];
-
-        public static int prevPosX;
-        public static int prevPosY;
-
-        public static bool placing = false;
-        public static bool voiding = false;
-        public static bool fire = false;
-
-        public static double mult = 0;
-
-        public static Material mP = Material.Sand;
-        public static string mPS;
-
-
-
 
         static void Main(string[] args)
         {
-            //Text text = new Text();
-
-            mult = _SWidth / _Width;
-            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(_Width, _Height), "Test");
+            RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(_Width, _Height), "Pixels");
             window.SetVerticalSyncEnabled(true);
 
             for (int y = 0; y < _Height; y++)
             {
                 for (int x = 0; x < _Width; x++)
                 {
-                    field[x, y] = new Pixel(Material.Air, x, y);
-                    fireField[x, y] = 0;
+                    field[x, y] = new Pixel(Air);
                 }
             }
             window.SetMouseCursorVisible(false);
-            prevPosX = 0;
-            prevPosY = 0;
-
-            //Font font = new Font(@"C:\Users\ameli\source\repos\Pixel-Pseudo-Physics\Resources\Fonts\Montserrat-Light.ttf");
+            //for (int y = 0; y < _Height; y += 10)
+            //{
+            //    for (int x = 0; x < _Width; x += 10)
+            //    {
+            //        field[x, y].color = Color.Blue;
+            //    }
+            //}
+            field[10, 10] = new Pixel(Sand);
             while (window.IsOpen)
             {
                 Update();
@@ -78,87 +59,33 @@ namespace SFMLTryout
                 window.Clear();
                 window.DispatchEvents();
 
-                //text = new Text(mPS, font);
-                //text.FillColor = Color.White;
-                //text.CharacterSize = 8;
-
                 Sprite mainviewport = new Sprite(MainViewPort);
                 window.Draw(mainviewport);
-                //window.Draw(text);
                 window.Display();
 
 
-                position = new Vector2i(Mouse.GetPosition(window).X / Convert.ToInt32(mult), Mouse.GetPosition(window).Y / Convert.ToInt32(mult));
 
-
-
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Space))
-                    placing = !placing;
-                if (Mouse.IsButtonPressed(Mouse.Button.Left))
-                    placing = true;
-                else
-                    placing = false;
-
-                //if (Mouse.IsButtonPressed(Mouse.Button.Right))
-                //ScreenBuffer[Mouse.GetPosition(window).X / Convert.ToInt32(mult),
-                //Mouse.GetPosition(window).Y / Convert.ToInt32(mult) + 1] = Color.Magenta;
-
-
-
-                if (Keyboard.IsKeyPressed(Keyboard.Key.V))
-                    voiding = !voiding;
-                if (Keyboard.IsKeyPressed(Keyboard.Key.F))
-                    fire = !fire;
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Num1))
-                    mP = Material.Sand;
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Num2))
-                    mP = Material.Water;
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Num3))
-                    mP = Material.Gas;
-                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
-                    window.Close();
-                if (Keyboard.IsKeyPressed(Keyboard.Key.F5))
-                    for (int y = 0; y < _Height; y++)
-                    {
-                        for (int x = 0; x < _Width; x++)
-                        {
-                            field[x, y] = new Pixel(Material.Air, x, y);
-                            fireField[x, y] = 0;
-                        }
-                    }
-                field[prevPosX, prevPosY] = new Pixel(Material.Air, prevPosX, prevPosY);
-
-
-                if (Mouse.GetPosition().X / mult < window.Size.X & Mouse.GetPosition().Y / mult < window.Size.Y)
-                    if (Mouse.GetPosition(window).X > 0 & Mouse.GetPosition(window).Y > 0)
-                    {
-                        prevPosY = Mouse.GetPosition(window).Y / Convert.ToInt32(mult);
-                        prevPosX = Mouse.GetPosition(window).X / Convert.ToInt32(mult);
-                        field[Mouse.GetPosition(window).X / Convert.ToInt32(mult), Mouse.GetPosition(window).Y / Convert.ToInt32(mult)] = new Pixel(Material.Generator, Mouse.GetPosition(window).X / Convert.ToInt32(mult), Mouse.GetPosition(window).Y / Convert.ToInt32(mult));
-                    }
-
-
-
-                if (mP == Material.Sand)
-                    mPS = "Sand ";
-                if (mP == Material.Water)
-                    mPS = "Water";
-                if (mP == Material.Gas)
-                    mPS = "Gas  ";
-
-                if (Mouse.GetPosition().X / mult < window.Size.X & Mouse.GetPosition().Y / mult < window.Size.Y)
-                    if (Mouse.GetPosition(window).X > 0 & Mouse.GetPosition(window).Y > 0)
-                        Draw();
-
+                if (true)
+                {
+                    Draw();
+                    //Drawing function
+                }
             }
         }
 
-
         static void Draw()
         {
-            for (int y = _Height - 1; y > 0; y--)
-                for (int x = 0; x < _Width; x++)
-                    field[x, y].Update();
+            for (uint y = _Height-1; y > 0; y--)
+            {
+                for (uint x = 0; x < _Width; x++)
+                {
+                    if (x == 10 & y == 10)
+                    {
+                        int i = 0;
+                    }
+                    field[x, y].Update(x, y);
+                }
+            }
         }
 
 
@@ -167,14 +94,14 @@ namespace SFMLTryout
             TestCounter++;
             if (TestCounter > _Width - 1) { TestCounter = 0; }
 
-            for (int x = 0; x < _Width; x++)
+            for (uint x = 0; x < _Width; x++)
             {
-                for (int y = 0; y < _Height; y++)
+                for (uint y = 0; y < _Height; y++)
                 {
-                    cpixels[x + (_Width * y)] = field[x, y].color;//make an intermediary array the correct dimention and arrange the pixels in the correct position to be drawn (separate step to keep everything clean, I find this operation incredibly confusing mainly because I had no idea how the pixels are supposed to be arrenged in the first place(still kind of dont))
+                    cpixels[x + (_Width * y)] = field[x, y].color;
                 }
             }
-            for (int i = 0; i < _Width * _Height * 4; i += 4)//fill the byte array
+            for (int i = 0; i < _Width * _Height * 4; i += 4)
             {
 
                 pixels[i + 0] = cpixels[i / 4].R;
@@ -183,7 +110,7 @@ namespace SFMLTryout
                 pixels[i + 3] = cpixels[i / 4].A;
             }
 
-            MainViewPort.Update(pixels);//update the texture with the array
+            MainViewPort.Update(pixels);
         }
     }
 }
